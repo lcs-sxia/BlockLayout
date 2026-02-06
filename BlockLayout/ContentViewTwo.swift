0d295a2cea305522343f78dcc5cf4b7581744942
Steven Xia <sxia@lcs.on.ca>
2026-02-06 09:20:18 -0500

    Completed second layout.

---
+++ BlockLayout/ContentViewTwo.swift
@@ -0,0 +1,18 @@
+//
+//  ContentViewTwo.swift
+//  BlockLayout
+//
+//  Created by Steven Xia on 2026/2/6.
+//
+
+import SwiftUI
+
+struct ContentViewTwo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentViewTwo()
+}
