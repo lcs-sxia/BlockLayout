a08230318eebf71018f3138cf08399b274f16214
Steven Xia <sxia@lcs.on.ca>
2026-02-06 08:50:05 -0500

    Initial Commit

---
+++ BlockLayout/BlockLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockLayoutApp.swift
+//  BlockLayout
+//
+//  Created by Steven Xia on 2026/2/6.
+//
+
+import SwiftUI
+
+@main
+struct BlockLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
